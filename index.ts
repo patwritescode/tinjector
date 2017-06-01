3b17add9ebea174d32f203a0927a5ec1a78033c9
Patrick Dunn <pat.dunn@outlook.com>
2017-06-01 00:41:55 -0400

    compile to es5. inject on initialization

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import {container} from "./src/container";
 import {define, definition} from "./src/runtimeInterface";
-import inject from "./src/inject";
+import {inject} from "./src/inject";
 import "reflect-metadata";
 
 export {
--- src/inject.ts
+++ src/inject.ts
@@ -1,33 +1,19 @@
 import {container} from "./container";
-export default function inject(target: Function | any, key?: string) {
+export function inject(target: Function | any, key?: string): any {
     let types = null;
+    // is a prop inject
     if (key) {
         types = Reflect.getMetadata("design:paramtypes", target, key);
     }
+    // is a constructor inject
     else {
         types = Reflect.getMetadata("design:paramtypes", target);
-        var s = types.map(a => a.name).join();
         // create a new copy of the constructor with the parameters fulfilled
-        // TODO: bind on types instead of just static test
-        console.log(`param types: ${s}`);
-        // const newConstructor: any = () => {
-        //     const newObj = new target(container.resolve(types[0]));
-            
-        //     return newObj;
-        // }
-        // newConstructor.prototype = target.prototype;
-        
-        // const newConstructor = (...args) => {
-        //     Object.assign(this, container.resolve(types[0]));
-        //     return target.apply(this, args);
-        // }
-        // newConstructor.prototype = target.prototype;
-        // return newConstructor;
-
-        //  return Object.assign(target, container.resolve(types[0]));
-        
-        return target.bind(target, ...types.map(type => container.resolve(type)));
+        const original = target;
+        const newConstructor = function(...args) {
+            return original.apply(this, types.map(type => container.resolve(type)));
+        }
+        newConstructor.prototype = original.prototype;
+        return newConstructor;
     }
-    var typeString = types.map(a => a.name).join();
-    console.log(`${key || target.name} param types: ${typeString}`);
 }
