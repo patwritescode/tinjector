df5c370b33a9edec3a78de82b9e3f17c412103cc
Patrick Dunn <patwritescode@users.noreply.github.com>
2017-06-23 22:11:50 -0400

    update container logic

--- src/container.ts
+++ src/container.ts
@@ -3,9 +3,13 @@ import { Registration } from "./Models";
 export class Container {
     private registrations: Array<Registration> = [];
 
+    private createInstance<T>(classType: {new (): T} | Function): Function {
+        return Reflect.construct(classType, []);
+    }
+
     registerAbstraction<T, U extends T>(
-        abstractClass: (new (...args: any[]) => T),
-        implementedClass: { new (): U; },
+        abstractClass: (new (...args: any[]) => T) | Function,
+        implementedClass: { new (...args: any[]): U; },
         singleton: boolean = false): Container {
         let registration = new Registration();
         registration.RegisteredClass = implementedClass;
@@ -22,10 +26,6 @@ export class Container {
         return this;
     }
 
-    private createInstance<T>(classType: {new (): T} | Function): Function {
-        return Reflect.construct(classType, []);
-    }
-
     resolve<T extends Function>(itemToResolve: (new (...args: any[]) => T) | Function): T {
         const resolvedRegistration = this.registrations.filter(registration => registration.RegisteredClass == itemToResolve || registration.RegisteredInterface == itemToResolve)[0];
         return this.createInstance(resolvedRegistration.RegisteredClass) as T;
@@ -33,19 +33,3 @@ export class Container {
 }
 
 export const container: Container = new Container();
-
-abstract class IPerson {
-    abstract doSomething(): void;
-}
-
-class Person implements IPerson {
-    doSomething(): void {
-        console.log("test");
-    }
-}
-
-class OtherClass {
-
-}
-
-container.registerAbstraction(OtherClass, Person);
