adbccfad7011b736c8959622741789c637f6ab0f
Patrick Dunn <patwritescode@users.noreply.github.com>
2017-06-29 21:26:23 -0400

    version bump. verify registrations.

--- src/Container.ts
+++ src/Container.ts
@@ -7,11 +7,19 @@ export class Container {
         return Reflect.construct(classType, []);
     }
 
-    registerAbstraction<T, U extends T>(
-        abstractClass: (new (...args: any[]) => T) | Function,
-        implementedClass: { new (...args: any[]): U; },
-        singleton: boolean = false): Container {
-        let registration = new Registration();
+    private isItemRegistered(item: any) : boolean {
+        const registrationMatch = this.registrations.filter(registration => registration.RegisteredInterface == item || registration.RegisteredClass == item)[0];
+        return registrationMatch != null;
+    }
+
+    registerAbstraction<T, U extends T>(abstractClass: (new (...args: any[]) => T) | Function, implementedClass: { new (...args: any[]): U; }, singleton: boolean = false): Container {
+        const existingRegistration = this.registrations
+            .filter(existingRegistration => 
+                existingRegistration.RegisteredClass == implementedClass || 
+                existingRegistration.RegisteredInterface == abstractClass);
+        if(this.isItemRegistered(abstractClass)) throw `You cannot register an abstract class twice`;
+        if(this.isItemRegistered(implementedClass)) throw `You cannot register an implemented class twice`;
+        const registration = new Registration();
         registration.RegisteredClass = implementedClass;
         registration.RegisteredInterface = abstractClass;
         this.registrations.push(registration);
@@ -19,7 +27,8 @@ export class Container {
     }
 
     registerClass<T>(implementedClass: { new (): T }, singleton: boolean = false): Container {
-        let registration = new Registration();
+        if(this.isItemRegistered(implementedClass)) throw `You cannot register a class more than once`;
+        const registration = new Registration();
         registration.RegisteredClass = implementedClass;
         registration.SingletonReference = singleton ? this.createInstance(implementedClass) : null;
         this.registrations.push(registration);
@@ -28,6 +37,7 @@ export class Container {
 
     resolve<T>(itemToResolve: (new (...args: any[]) => T) | Function): T {
         const resolvedRegistration = this.registrations.filter(registration => registration.RegisteredClass == itemToResolve || registration.RegisteredInterface == itemToResolve)[0];
+        if(!resolvedRegistration) throw `No matching implementation was registered.`;
         return this.createInstance(resolvedRegistration.RegisteredClass as Function) as T;
     }
 }
